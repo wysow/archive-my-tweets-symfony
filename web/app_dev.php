88210de6bb0907e68ced70841c62931ae79ca1cf
Gaultier BONIFACE <gboniface@wysow.fr>
2013-07-08 10:42:18 +0200

    allow debug on pagodabox

--- web/app_dev.php
+++ web/app_dev.php
@@ -13,6 +13,21 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
 ) {
+    // to allow access on pagodabox
+    if(isset($_SERVER['APP_NAME']) && $_SERVER['APP_NAME'] == 'archive-my-tweets-wysow-fr') {
+        $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+        Debug::enable();
+
+        require_once __DIR__.'/../app/AppKernel.php';
+
+        $kernel = new AppKernel('dev', true);
+        $kernel->loadClassCache();
+        Request::enableHttpMethodParameterOverride();
+        $request = Request::createFromGlobals();
+        $response = $kernel->handle($request);
+        $response->send();
+        $kernel->terminate($request, $response);
+    }
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
