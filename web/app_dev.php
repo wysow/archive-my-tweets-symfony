707b81e9b80b46a45b03713bb856eafdf89d17d1
Gaultier Boniface <gboniface@wysow.fr>
2014-01-25 23:20:51 +0100

    Fixing bug when search term is empty Updating front controllers Updating Boxfile for Pagodaboxwq!

--- web/app_dev.php
+++ web/app_dev.php
@@ -13,21 +13,6 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
 ) {
-    // to allow access on pagodabox
-    if(isset($_SERVER['APP_DEBUG']) && $_SERVER['APP_DEBUG'] == '1') {
-        $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-        Debug::enable();
-
-        require_once __DIR__.'/../app/AppKernel.php';
-
-        $kernel = new AppKernel('dev', true);
-        $kernel->loadClassCache();
-        Request::enableHttpMethodParameterOverride();
-        $request = Request::createFromGlobals();
-        $response = $kernel->handle($request);
-        $response->send();
-        $kernel->terminate($request, $response);
-    }
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
@@ -39,7 +24,6 @@ require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
-Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
