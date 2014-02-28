8e1abeaf086a6f5fe7ef47aca2c129acf0ffce4e
Gaultier Boniface <gboniface@wysow.fr>
2014-02-28 16:27:42 +0100

    Updating pagodabox hook

--- bin/after_deploy.sh
+++ bin/after_deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 PHP=php
 
+echo 'Update the database schema'
+$PHP app/console doctrine:schema:update --force
+
 echo 'Clear the cache of the production environment.'
 $PHP app/console cache:clear --env=prod
